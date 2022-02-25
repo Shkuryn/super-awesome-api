c9da4464cf921c5e4caaf4216e4d66fada675e8e
Igor <shkuryn@gmail.com>
2022-02-25 09:18:48 +0300

    users

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module SuperAwesomeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use Rack::Attack
+    #config.middleware.use Rack::Attack
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_190938) do
+ActiveRecord::Schema.define(version: 2022_02_24_063128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_02_22_190938) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "token"
+    t.string "email"
+    t.string "password"
     t.index ["token"], name: "index_users_on_token"
   end
 
